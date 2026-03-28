e8e3a12b251c7351ff211b26e91b07dd78b769ea
Ismail Mahesha <ismailmahesha12@gmail.com>
2026-03-28 20:31:22 +0300

    Update project files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src', // location of index.html and main entry
   build: {
     outDir: '../dist', // ensure output goes to repo/dist so Netlify can publish it
     emptyOutDir: true,
