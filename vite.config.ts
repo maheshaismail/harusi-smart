0a08e602c53f160c576a265ddd295c519d5e9437
Ismail Mahesha <ismailmahesha12@gmail.com>
2026-03-28 20:39:39 +0300

    Fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir: '../dist', // ensure output goes to repo/dist so Netlify can publish it
+    outDir: 'dist', // ensure output goes to repo/dist so Netlify can publish it
     emptyOutDir: true,
   },
 });
